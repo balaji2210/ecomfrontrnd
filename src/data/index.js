5f407a35f4959fe0b3e67b7d6f59352035996bae
balaji2210 <balajiatkuri10@gmail.com>
2023-01-22 20:49:47 +0530

    refactor(url): change url

--- src/data/index.js
+++ src/data/index.js
@@ -1,12 +1,15 @@
 export const signup = (user) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/signup`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/signup`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -27,14 +30,17 @@ export const isAuthenticated = () => {
 };
 
 export const signin = (user) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/signin`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/signin`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -44,9 +50,12 @@ export const signin = (user) => {
 };
 
 export const getAllproduct = (page) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/products/?page=${page}`, {
-    method: "GET",
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/products/?page=${page}`,
+    {
+      method: "GET",
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -56,9 +65,12 @@ export const getAllproduct = (page) => {
 };
 
 export const getAProduct = (id) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/product/${id}`, {
-    method: "GET",
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/product/${id}`,
+    {
+      method: "GET",
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -68,15 +80,18 @@ export const getAProduct = (id) => {
 };
 
 export const addReview = (review, id, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/review/${id}`, {
-    method: "PUT",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(review),
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/review/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(review),
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -84,21 +99,27 @@ export const addReview = (review, id, token) => {
 };
 
 export const getReviews = (id) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/reviews?id=${id}`, {
-    method: "GET",
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/reviews?id=${id}`,
+    {
+      method: "GET",
+    }
+  )
     .then((res) => {
       return res.json();
     })
     .catch((err) => console.log(err));
 };
 export const deleteReviews = (id, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/review?id=${id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/review?id=${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -109,7 +130,7 @@ export const deleteReviews = (id, token) => {
 
 export const search = (search) => {
   return fetch(
-    `https://ecom2210.herokuapp.com/api/v1/products?search=${search}`,
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/products?search=${search}`,
     {
       method: "GET",
     }
@@ -155,15 +176,18 @@ export const removeItemCart = (itemId, next) => {
 };
 
 export const createOrder = (token, data) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/order/create`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/order/create`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -171,12 +195,15 @@ export const createOrder = (token, data) => {
 };
 
 export const getMyOrders = (token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/orders`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/orders`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -184,15 +211,18 @@ export const getMyOrders = (token) => {
 };
 
 export const updatePassword = (pass, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/update/password`, {
-    method: "PUT",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(pass),
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/update/password`,
+    {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(pass),
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -200,12 +230,15 @@ export const updatePassword = (pass, token) => {
 };
 
 export const adminGetOrders = (token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/admin/orders`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/admin/orders`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -213,12 +246,15 @@ export const adminGetOrders = (token) => {
 };
 
 export const adminUpdateOrder = (id, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/admin/order/${id}`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/admin/order/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -226,12 +262,15 @@ export const adminUpdateOrder = (id, token) => {
 };
 
 export const getDeliveredOrders = (token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/admin/orders/delivered`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/admin/orders/delivered`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -240,7 +279,7 @@ export const getDeliveredOrders = (token) => {
 
 export const getProcessing = (token) => {
   return fetch(
-    `https://ecom2210.herokuapp.com/api/v1/admin/orders/processing`,
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/admin/orders/processing`,
     {
       method: "GET",
       headers: {
@@ -255,13 +294,16 @@ export const getProcessing = (token) => {
 };
 
 export const addProduct = (formdata, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/create/product`, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: formdata,
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/create/product`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formdata,
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -269,13 +311,16 @@ export const addProduct = (formdata, token) => {
 };
 
 export const updateProduct = (id, formdata, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/product/update/${id}`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    body: formdata,
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/product/update/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formdata,
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -283,12 +328,15 @@ export const updateProduct = (id, formdata, token) => {
 };
 
 export const deleteAProduct = (id, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/product/delete/${id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/product/delete/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -296,12 +344,15 @@ export const deleteAProduct = (id, token) => {
 };
 
 export const adminGetUsers = (token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/admin/users`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/admin/users`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
@@ -309,12 +360,15 @@ export const adminGetUsers = (token) => {
 };
 
 export const adminUpdateUser = (id, token) => {
-  return fetch(`https://ecom2210.herokuapp.com/api/v1/admin/user/${id}`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
+  return fetch(
+    `https://ecombackend-production-2b00.up.railway.app/api/v1/admin/user/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
     .then((res) => {
       return res.json();
     })
